b9512a2a7fa5e2e5d087c73fc476da51e01853ca
youthtrouble <harjhibolar@gmail.com>
2021-01-25 12:09:43 +0100

    funnay main

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Welcome to my awessoe site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":8000", nil)
+}
